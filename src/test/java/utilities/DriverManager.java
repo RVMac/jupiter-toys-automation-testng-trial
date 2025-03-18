4d7474b828cfa3933bbc00b32c8f110158c1961d
RVMac <rovenmacapagal@gmail.com>
2025-03-18 22:24:57 +0800

    added ChromeOptions for ChromeDriver

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -11,9 +12,13 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver.get() == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Enable headless mode
+            options.addArguments("--disable-gpu");
+
             WebDriverManager.chromedriver().setup();
 
-            driver.set(new ChromeDriver());
+            driver.set(new ChromeDriver(options));
             driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.get().manage().window().maximize();
         }
